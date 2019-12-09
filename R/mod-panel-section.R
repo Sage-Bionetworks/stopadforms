58dd4a777e7f5a7027e659879290c418a91af61e
Nicole Kauer <nmekauer@gmail.com>
2019-12-09 12:02:47 -0800

    Add show_reviews_table() to setup and show table

--- R/mod-panel-section.R
+++ R/mod-panel-section.R
@@ -56,12 +56,49 @@ mod_panel_section_server <- function(input, output, session, synapse, syn,
     "submission",
     choices = c("", unique(reviews()$submission))
   )
+  show_review_table(input, output, reviews, submission)
 
+  observeEvent(input$refresh_comments, {
+    dccvalidator::with_busy_indicator_server("refresh_comments", {
+      reviews <<- reactive(pull_reviews_table(syn, reviews_table))
+      updateSelectInput(
+        session = getDefaultReactiveDomain(),
+        "submission",
+        choices = c("", unique(reviews()$submission)),
+        selected = submission()
+      )
+      show_review_table(input, output, reviews, submission)
+    })
+  })
+}
+
+#' @title Pull latest review table
+#' 
+#' @description Pull latest review table from Synapse.
+#' 
+#' @inheritParams mod_panel_section_server
+#' @keywords internal
+pull_reviews_table <- function(syn, reviews_table) {
+  reviews <- syn$tableQuery(glue::glue("SELECT * FROM {reviews_table}"))
+  reviews <- readr::read_csv(reviews$filepath) %>%
+    dplyr::mutate(scorer = get_display_name(syn, .data$scorer))
+  reviews
+}
+
+#' @title Show review table
+#'
+#' @description Show review table.
+#'
+#' @inheritParams mod_panel_section_server
+#' @param reviews Dataframe review table.
+#' @param submission Submission name.
+#' @keywords internal
+show_review_table <- function(input, output, reviews, submission) {
   to_show <- reactive({
     dplyr::filter(reviews(), submission == submission()) %>%
       dplyr::select(.data$section, .data$score, .data$scorer, .data$comments)
   })
-
+  
   output$averaged_scores <- reactable::renderReactable({
     reactable::reactable(
       to_show(),
@@ -78,51 +115,4 @@ mod_panel_section_server <- function(input, output, session, synapse, syn,
       )
     )
   })
-
-  observeEvent(input$refresh_comments, {
-    dccvalidator::with_busy_indicator_server("refresh_comments", {
-      reviews <<- reactive(pull_reviews_table(syn, reviews_table))
-      updateSelectInput(
-        session = getDefaultReactiveDomain(),
-        "submission",
-        choices = c("", unique(reviews()$submission)),
-        selected = submission()
-      )
-
-      to_show <- reactive({
-        dplyr::filter(reviews(), submission == submission()) %>%
-          dplyr::select(.data$section, .data$score, .data$scorer, .data$comments)
-      })
-
-      output$averaged_scores <- reactable::renderReactable({
-        reactable::reactable(
-          to_show(),
-          groupBy = "section",
-          searchable = TRUE,
-          columns = list(
-            section = reactable::colDef(name = "Section"),
-            score = reactable::colDef(name = "Score", aggregate = "mean"),
-            scorer = reactable::colDef(name = "Scorer(s)", aggregate = "unique"),
-            comments = reactable::colDef(
-              name = "Comments",
-              aggregate = reactable::JS("function(values, rows) { return '...' }")
-            )
-          )
-        )
-      })
-    })
-  })
-}
-
-#' @title Pull latest review table
-#' 
-#' @description Pull latest review table from Synapse.
-#' 
-#' @inheritParams mod_panel_section
-#' @keywords internal
-pull_reviews_table <- function(syn, reviews_table) {
-  reviews <- syn$tableQuery(glue::glue("SELECT * FROM {reviews_table}"))
-  reviews <- readr::read_csv(reviews$filepath) %>%
-    dplyr::mutate(scorer = get_display_name(syn, .data$scorer))
-  reviews
 }
