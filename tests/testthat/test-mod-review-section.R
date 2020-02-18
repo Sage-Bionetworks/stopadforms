e0138dc1542713a783f521eafd9442cf97be6f9f
Kara Woo <kara.woo@sagebase.org>
2020-02-17 16:23:55 -0800

    Add tests

---
+++ tests/testthat/test-mod-review-section.R
@@ -0,0 +1,40 @@
+context("mod-review-section.R")
+
+test_that("reorder_steps puts steps in correct order", {
+  test <- c(
+    "Efficacy",
+    "Naming",
+    "LD50 [1]",
+    "LD50 [2]",
+    "Clinical Data [1]",
+    "Measurements",
+    "Acute Dosing"
+  )
+  expect_identical(
+    reorder_steps(test),
+    c(
+      "Naming",
+      "Measurements",
+      "Efficacy",
+      "LD50 [1]",
+      "LD50 [2]",
+      "Acute Dosing",
+      "Clinical Data [1]"
+    )
+  )
+})
+
+test_that("reorder_steps puts any unexpected sections at the end", {
+  test <- c("nope", "LD50", "also no")
+  expect_identical(
+    reorder_steps(test),
+    c("LD50", "nope", "also no")
+  )
+})
+
+test_that("reorder_steps doesn't fail even if no sections match", {
+  expect_identical(
+    reorder_steps(c("a", "b")),
+    c("a", "b")
+  )
+})
