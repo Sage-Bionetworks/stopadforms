c634390a51564799ea023d945c789da723a831e1
Kara Woo <kara.woo@sagebase.org>
2020-04-16 17:15:38 -0700

    Rearrange and add tests

--- tests/testthat/test-data-gather-clean.R
+++ tests/testthat/test-data-gather-clean.R
@@ -18,39 +18,6 @@ data1 <- tibble::tibble(
   response = c("3", "Smith", "Exp 1", "TRUE", "0")
 )
 
-test_that("map_sections_variables() maps correctly", {
-  expected <- tibble::tibble(
-    section = c("basic", "naming", "pk_in_vivo", "pk_in_vivo", "pk_in_vivo"),
-    variable = c("age_range_min", "last_name", "name", "route1", "is_solution"),
-    exp_num = c(NA, NA, 1, 2, 1),
-    response = c("3", "Smith", "Exp 1", "TRUE", "0"),
-    step = c("Basic Data", "Naming", "Pk in Vivo [1]", "Pk in Vivo [2]",
-             "Pk in Vivo [1]"),
-    label = c("Min of age range", "Last name", "Experiment Name",
-              "route1", "Is compound a solution?")
-  )
-  res <- map_sections_variables(data1, lookup_table)
-  expect_equal(res, expected)
-})
-
-test_that("change_logical_responses() fixes responses to yes/no", {
-  data <- tibble::tibble(
-    variable = c("is_solution", "is_solution", "is_solution", "is_compound"),
-    response = c("0", "1", "TRUE", "FALSE")
-  )
-  res <- change_logical_responses(data)
-  expect_equal(res$response, c("No", "Yes", "Yes", "No"))
-})
-
-test_that("change_logical_responses() changes correct rows", {
-  data <- tibble::tibble(
-    variable = c("name", "species", "is_solution", "is_solution"),
-    response = c("foo", "mouse", "0", "1")
-  )
-  res <- change_logical_responses(data)
-  expect_equal(res$response, c("foo", "mouse", "No", "Yes"))
-})
-
 ## Sample JSON data to test with
 json <- '
 {
@@ -91,7 +58,14 @@ json <- '
   }
 }
 '
-## Convert to list
+
+# get_submissions() ------------------------------------------------------------
+
+get_submissions()
+
+# create_section_table() -------------------------------------------------------
+
+# Convert sample JSON to list
 dat_list <- jsonlite::fromJSON(json, simplifyDataFrame = FALSE)
 
 test_that("create_section_table creates rows for each response", {
@@ -137,3 +111,55 @@ test_that("create_section_table returns multiple selections from responses", {
     c("oral", "injection", "transdermal", "formulated_in_food")
   )
 })
+
+# create_values_table() --------------------------------------------------------
+
+test_that("create_values_table turns sub-list into tibble", {
+  res <- create_values_table(dat_list[[1]], section = names(dat_list[1]))
+  expected <- tibble::tibble(
+    section = "pk_in_vitro",
+    exp_num = NA,
+    variable = "permeability",
+    response = "super permeable"
+  )
+  expect_identical(res, expected)
+})
+
+# change_logical_responses() ---------------------------------------------------
+
+test_that("change_logical_responses() fixes responses to yes/no", {
+  data <- tibble::tibble(
+    variable = c("is_solution", "is_solution", "is_solution", "is_compound"),
+    response = c("0", "1", "TRUE", "FALSE")
+  )
+  res <- change_logical_responses(data)
+  expect_equal(res$response, c("No", "Yes", "Yes", "No"))
+})
+
+test_that("change_logical_responses() changes correct rows", {
+  data <- tibble::tibble(
+    variable = c("name", "species", "is_solution", "is_solution"),
+    response = c("foo", "mouse", "0", "1")
+  )
+  res <- change_logical_responses(data)
+  expect_equal(res$response, c("foo", "mouse", "No", "Yes"))
+})
+
+
+# map_sections_variables() -----------------------------------------------------
+
+test_that("map_sections_variables() maps correctly", {
+  expected <- tibble::tibble(
+    section = c("basic", "naming", "pk_in_vivo", "pk_in_vivo", "pk_in_vivo"),
+    variable = c("age_range_min", "last_name", "name", "route1", "is_solution"),
+    exp_num = c(NA, NA, 1, 2, 1),
+    response = c("3", "Smith", "Exp 1", "TRUE", "0"),
+    step = c("Basic Data", "Naming", "Pk in Vivo [1]", "Pk in Vivo [2]",
+             "Pk in Vivo [1]"),
+    label = c("Min of age range", "Last name", "Experiment Name",
+              "route1", "Is compound a solution?")
+  )
+  res <- map_sections_variables(data1, lookup_table)
+  expect_equal(res, expected)
+})
+
