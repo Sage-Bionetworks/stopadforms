f0211383d5091342795e2fbd625354ac4b8895f7
Kara Woo <karawoo@users.noreply.github.com>
2021-03-04 15:01:42 -0800

    Add more detail about scoring (#86)
    * Export lookup table and partial beta weight table
    
    * Add example to README that uses sample data included in package
    
    * Remove demo script from inst/
    
    This information is now more visible in the README examples
    
    * Add scoring vignette

--- data-raw/lookup_table.R
+++ data-raw/lookup_table.R
@@ -1,4 +1,4 @@
 ## code to prepare `lookup_table` dataset goes here
 lookup_table <- readr::read_csv("stopad_lookup_table.csv")
 partial_betas <- readr::read_csv("stopad_beta_weights.csv")
-usethis::use_data(lookup_table, partial_betas, internal = TRUE, overwrite = TRUE)
+usethis::use_data(lookup_table, partial_betas, internal = FALSE, overwrite = TRUE)
