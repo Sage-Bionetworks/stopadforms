e8897c9b92c1c28adb916c474cdca212ecdcb0e3
Nicole Kauer <nmekauer@gmail.com>
2021-07-13 10:05:22 -0700

    Add config with app_urls and needed functions

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+Sys.setenv(R_CONFIG_ACTIVE = "default") # Replace "default" with your config
 pkgload::load_all()
 options("golem.app.prod" = TRUE)
 stopadforms::run_app() # add parameters here (if any)
