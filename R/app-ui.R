1594d0660e9592c7be1db430c0b3db764e3bf9ad
Jared Hendrickson <jared.hendrickson@sagebase.org>
2022-07-01 15:08:36 -0700

    add comments to recent bug fixes

--- R/app-ui.R
+++ R/app-ui.R
@@ -14,6 +14,7 @@ golem_add_external_resources <- function() {
     "www", system.file("app/www", package = "stopadforms")
   )
 
+  #hack fix for refresh error
   tags$head(
     golem::activate_js(),
     golem::favicon(),
